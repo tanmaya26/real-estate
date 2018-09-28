5d3ee914afc82feb80cb172c03b422ddcd90eafe
Mohit Jain <mjain6@ncsu.com>
2018-09-27 23:15:15 -0400

    Merge Conflict Resolve

--- HouseHunter/db/schema.rb
+++ HouseHunter/db/schema.rb
@@ -10,8 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_28_003521) do
-
+ActiveRecord::Schema.define(version: 2018_09_28_005257) do
+  
   create_table "companies", force: :cascade do |t|
     t.text "name"
     t.text "website"
@@ -23,23 +23,22 @@ ActiveRecord::Schema.define(version: 2018_09_28_003521) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
-  create_table "houses", force: :cascade do |t|
+  
+  create_table "company_house_mappings", force: :cascade do |t|
     t.integer "company_id"
-    t.string "location"
-    t.integer "square_footage"
-    t.integer "year_built"
-    t.string "style"
-    t.float "price_list"
-    t.integer "number_of_floors"
-    t.boolean "basement"
-    t.string "current_owner"
-    t.string "contact_info_realtor"
+    t.integer "house_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["company_id"], name: "index_houses_on_company_id"
+    t.index ["company_id"], name: "index_company_house_mappings_on_company_id"
+    t.index ["house_id"], name: "index_company_house_mappings_on_house_id"
   end
-
+  
+  create_table "roles", force: :cascade do |t|
+    t.text "role_type"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+  
   create_table "users", force: :cascade do |t|
     t.text "user_name"
     t.text "password"
