236a74dedb8b58de49e029990e8432920a4215ab
Mohit Jain <mjain6@ncsu.edu>
2018-09-27 23:49:34 -0400

    Update routes.rb

--- HouseHunter/config/routes.rb
+++ HouseHunter/config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
-  resources :company_house_mappings
+  
   get 'home/index'
   resources :companies
   resources :roles
   resources :users
   resources :houses
+  resources :house_realtors
+  resources :company_house_mappings
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#index'
 end
+
