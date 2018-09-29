b1febeb15d22434a304b6e17d58c4af7294afecf
Mohit Jain <mjain6@ncsu.com>
2018-09-29 14:01:55 -0400

    Adding user ref in House & deleting house relator model

---
+++ HouseHunter/db/migrate/20180929175714_add_user_ref_to_house.rb
@@ -0,0 +1,5 @@
+class AddUserRefToHouse < ActiveRecord::Migration[5.2]
+  def change
+    add_reference :houses, :user, foreign_key: true
+  end
+end
--- HouseHunter/db/schema.rb
+++ HouseHunter/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_29_134006) do
+ActiveRecord::Schema.define(version: 2018_09_29_175714) do
 
   create_table "companies", force: :cascade do |t|
     t.string "name"
@@ -22,15 +22,6 @@ ActiveRecord::Schema.define(version: 2018_09_29_134006) do
     t.integer "size"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "house_realtors", force: :cascade do |t|
-    t.integer "house_id"
-    t.integer "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["house_id"], name: "index_house_realtors_on_house_id"
-    t.index ["user_id"], name: "index_house_realtors_on_user_id"
   end
 
   create_table "houses", force: :cascade do |t|
@@ -46,7 +37,9 @@ ActiveRecord::Schema.define(version: 2018_09_29_134006) do
     t.string "contact_info_realtor"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
     t.index ["company_id"], name: "index_houses_on_company_id"
+    t.index ["user_id"], name: "index_houses_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
